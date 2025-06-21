59730f808d5226952193656f0d423afa33f93cf9
Somaya <shosmayyy@gmail.com>
2025-06-21 04:56:11 +0000

    [automatic commit] 2025-06-21 04:56:10 UTC 125.253.49.185

--- part2/app.js
+++ part2/app.js
@@ -5,30 +5,27 @@ require('dotenv').config();
 
 const app = express();
 
-app.use('/', require('./routes/index'));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// session
 app.use(session({
   secret: 'secretkey',
   resave: false,
   saveUninitialized: false
 }));
 
-// serve views
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
+const indexRoutes = require('./routes/index'); //
 
+app.use('/', indexRoutes);
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
+// Static files come last so / doesn't get hijacked by index.html
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Export the app instead of listening here
 module.exports = app;
