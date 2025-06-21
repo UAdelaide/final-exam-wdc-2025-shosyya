1db5d6bc52d2f5f75360c6bfee4d0d51cb63eff7
Somaya <shosmayyy@gmail.com>
2025-06-21 04:58:23 +0000

    [automatic commit] 2025-06-21 04:58:22 UTC 125.253.49.185

--- part2/routes/index.js
+++ part2/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db');
+const db = require('../models/db');
 
 // homepage with login form
 router.get('/', (req, res) => {
