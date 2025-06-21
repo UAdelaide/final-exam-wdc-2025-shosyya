2d8c5bdbf066d6e01033ab5657a736d45797f974
Somaya <shosmayyy@gmail.com>
2025-06-21 00:30:30 +0000

    [automatic commit] 2025-06-21 00:30:30 UTC 125.253.49.185

--- starthere/routes/index.js
+++ starthere/routes/index.js
@@ -6,13 +6,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* Login form HTML */
-npm install<form method="POST" action="/login">
-  <label>Username:</label>
-  <input type="text" name="username" required><br>
-  <label>Password:</label>
-  <input type="password" name="password" required><br>
-  <button type="submit">Login</button>
-</form>
+
 
 module.exports = router;
