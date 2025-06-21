bbedd82c4e9bc581772a4cf933ddf2b003839bca
Somaya <shosmayyy@gmail.com>
2025-06-21 04:31:54 +0000

    [automatic commit] 2025-06-21 04:31:54 UTC 125.253.49.185

--- part2/app.js
+++ part2/app.js
@@ -10,8 +10,6 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 // Session
 app.use(session({
   secret: 'secretkey',
