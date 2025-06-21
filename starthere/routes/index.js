8297156d69906549bc20a408a6a1fcf73ba5633a
Somaya <shosmayyy@gmail.com>
2025-06-21 00:01:13 +0000

    [automatic commit] 2025-06-21 00:01:13 UTC 125.253.49.185

--- starthere/routes/index.js
+++ starthere/routes/index.js
@@ -7,6 +7,13 @@ router.get('/', function(req, res, next) {
 });
 
 /* Login logic */
+npm install<form method="POST" action="/login">
+  <label>Username:</label>
+  <input type="text" name="username" required><br>
+  <label>Password:</label>
+  <input type="password" name="password" required><br>
+  <button type="submit">Login</button>
+</form>
 
 
 
