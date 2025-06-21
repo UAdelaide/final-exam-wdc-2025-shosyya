914f984a08c343403058629cd313f12bae0385a6
Somaya <shosmayyy@gmail.com>
2025-06-21 04:41:50 +0000

    [automatic commit] 2025-06-21 04:41:49 UTC 125.253.49.185

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -40,7 +40,7 @@ router.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const [rows] = await db.query(`
+    const [rows] = await db.execute(`
       SELECT user_id, username, role FROM Users
       WHERE email = ? AND password_hash = ?
     `, [email, password]);
